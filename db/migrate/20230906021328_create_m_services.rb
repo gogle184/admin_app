fea6620c80451a6f118d5799bafa8c5d5f804d64
gogle184 <gogle184@gmail.com>
2023-09-06 11:23:59 +0900

    add m_servicesモデルのマイグレーション

--- db/migrate/20230906021328_create_m_services.rb
+++ db/migrate/20230906021328_create_m_services.rb
@@ -1,12 +1,12 @@
 class CreateMServices < ActiveRecord::Migration[7.0]
   def change
     create_table :m_services do |t|
-      t.boolean :is_royalty
-      t.boolean :is_browsing_history
-      t.boolean :is_japan_concierge
-      t.boolean :is_ai_concierge
-      t.boolean :is_questionnaire
-      t.boolean :is_keyword
+      t.boolean :is_royalty, null: false, default: true, comment: "ロイヤリティ"
+      t.boolean :is_browsing_history, null: false, default: true, comment: "閲覧履歴"
+      t.boolean :is_japan_concierge, null: false, default: true, comment: "お気に入り履歴"
+      t.boolean :is_ai_concierge, null: false, default: true, comment: "AIコンシェルジュ"
+      t.boolean :is_questionnaire, null: false, default: true, comment: "募集アンケート"
+      t.boolean :is_keyword, null: false, default: true, comment: "キーワード"
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_072430) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_021328) do
+  create_table "m_services", force: :cascade do |t|
+    t.boolean "is_royalty", default: true, null: false
+    t.boolean "is_browsing_history", default: true, null: false
+    t.boolean "is_japan_concierge", default: true, null: false
+    t.boolean "is_ai_concierge", default: true, null: false
+    t.boolean "is_questionnaire", default: true, null: false
+    t.boolean "is_keyword", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
